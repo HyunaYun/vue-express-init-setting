1e36d30240152fbb4c496d609e791fe8448596c3
hyuna yoon <easy.hyuna@gmail.com>
2022-03-17 10:57:04 +0900

    project init setting [1. add vuex, axios]

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+/** AXIOS */
+import axios from "axios";
+Vue.prototype.$axios = axios;
+
+/** VUEX */
+import Vuex from "vuex";
+Vue.use(Vuex);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
